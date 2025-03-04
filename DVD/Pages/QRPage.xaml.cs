954c093fada7b95b2d8893e3ff13cc8d6e84d05a
Ыегвуте48 <gadelxaz@gmail.com>
2025-03-04 15:14:40 +0300

    Добавьте файлы проекта.

---
+++ DVD/Pages/QRPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Drawing;
+using QRCoder;
+
+namespace DVD.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для QRPage.xaml
+    /// </summary>
+    public partial class QRPage : Page
+    {
+        public QRPage()
+        {
+            InitializeComponent();
+        }
+
+        private void QrCode_Click(object sender, RoutedEventArgs e)
+        {
+            string source = "";
+
+        }
+    }
+}
